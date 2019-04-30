d0eecffdd4c4f5c291bd3095f5e7a3d79854fa71
Avik Nandi <aviknandi@yahoo.com>
2019-04-30 17:51:12 +0530

    Placed Ingredience into recipe-details component

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { IngredienceService } from '../common/ingredience.service';
+import { Ingredience } from '../ingredient/ingredience.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,11 +9,13 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() data;
+  ingredience:Ingredience[];
   
-  constructor(){
+  constructor(private ingredienceService:IngredienceService){
     
   }
   ngOnInit() {
+    this.ingredience = this.ingredienceService.getAllIngredience();
   }
 
 }
